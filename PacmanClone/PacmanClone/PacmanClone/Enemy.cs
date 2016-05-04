72723205485b31654a3ab3b7273d65670c3a02d9
Kyle Duggan <KyleDuggan1580@gmail.com>
2016-05-04 11:35:18 -0400

    Enemy Class

--- PacmanClone/PacmanClone/PacmanClone/Enemy.cs
+++ PacmanClone/PacmanClone/PacmanClone/Enemy.cs
@@ -13,9 +13,65 @@ namespace PacmanClone
 {
     class Enemy
     {
-        private Sprite enemy;
-        private Rectangle screenBounds;
-        private float shotTimer= 0.0f;
-        private float shotTimeMax = 1f;
+        private Sprite enemySprite;
+        public int level = 1;
+        public bool isDestroyed = false;
+        private int enemyRadius = 15;
+
+
+        public Enemy(Texture2D texture, Vector2 location, Rectangle initialFrame, Rectangle screenBounds)
+        {
+            enemySprite = new Sprite(
+                location,
+                texture,
+                initialFrame,
+                Vector2.Zero);
+
+            enemySprite.CollisionRadius = enemyRadius;
+        }
+
+
+
+        public void MoveRight()
+        {
+            enemySprite.Location += new Vector2(5, 0);
+        }
+        public void MoveLeft()
+        {
+            enemySprite.Location -= new Vector2(5, 0);
+        }
+        public void MoveUp()
+        {
+            enemySprite.Location -= new Vector2(0, 5);
+        }
+
+
+
+        public bool IsActive()
+        {
+            if (isDestroyed)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+
+        public void Update(GameTime gameTime)
+        {
+            if (IsActive())
+            {
+                enemySprite.Update(gameTime);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (IsActive())
+            {
+                enemySprite.Draw(spriteBatch);
+            }
+        }
     }
 }
