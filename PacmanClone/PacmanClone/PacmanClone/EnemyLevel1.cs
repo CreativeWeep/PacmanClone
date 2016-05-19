fedae401590d8e09beb97f0149104a579bfac15e
Kyle Duggan <KyleDuggan1580@gmail.com>
2016-05-19 11:45:33 -0400

    did something i guess

--- PacmanClone/PacmanClone/PacmanClone/EnemyLevel1.cs
+++ PacmanClone/PacmanClone/PacmanClone/EnemyLevel1.cs
@@ -16,6 +16,7 @@ namespace PacmanClone
         private int dir = 0;
         private float directionChangeTime = 0f;
         private Random rand;
+        private ShotManager enemyShotManager;
 
         public EnemyLevel1(Texture2D texture, Vector2 location, Rectangle initialFrame, Rectangle screenBounds, Random rand) : base(texture, location, initialFrame, screenBounds)
         {
@@ -23,6 +24,8 @@ namespace PacmanClone
             this.enemySprite.Velocity = new Vector2(0, 10);
             this.rand = rand;
             directionChangeTime = rand.Next(0, 1200);
+
+            
         }
 
         public override void Update(GameTime gameTime)
@@ -37,8 +40,8 @@ namespace PacmanClone
 
                 base.Update(gameTime);
 
-                this.enemySprite.Location = new Vector2(MathHelper.Clamp(this.enemySprite.Center.X, screenBounds.Left, screenBounds.Right),
-                                                                           MathHelper.Clamp(this.enemySprite.Center.Y, screenBounds.Top, screenBounds.Bottom));
+                this.enemySprite.Location = new Vector2(MathHelper.Clamp(this.enemySprite.Center.X, screenBounds.Left, screenBounds.Right)/1.13f,
+                                                                           MathHelper.Clamp(this.enemySprite.Center.Y, screenBounds.Top, screenBounds.Bottom)/1.11f);
 
                 if (!this.enemySprite.IsBoxColliding(screenBounds))
                 {
